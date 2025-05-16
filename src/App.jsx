2eab12d3b560c4cef261cf4b194e420b2e3c30b9
matos.leticia0208 <matos.leticia0208@gmail.com>
2025-05-16 00:20:03 +0000

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,32 @@ import { useState } from "react"
 
 export function App() {
   const [numero, setNumero] = useState(100)
-
-  function diminuir() {
-    setNumero(numero - 100)
-  }
-
+  const[mostrarEsconder, setMostrarEsconder] = useState(false)
+ 
   function aumentar() {
     setNumero(numero + 100)
   }
+
+  function handleMostrarEsconder() {
+    if (mostrarEsconder === true) {
+      setMostrarEsconder(false)
+      return
+    }
+    setMostrarEsconder(true)
+  }
+
   return(
     <div>
       <h1>Numero: {numero}</h1>
 
       <div>
-        <button onClick= {diminuir}>Diminuir</button>
         <button onClick= {aumentar}>Aumentar</button>
+      </div>
+
+      <div>
+       {mostrarEsconder === true ? <p>Parágrafo para esconder</p> : ""}
+
+       <button onClick={handleMostrarEsconder}>{mostrarEsconder===true ? "Esconder" : "Mostrar"}</button>
       </div>
     </div>
   )
